dc41a0c489ca2854047fd0b213e88e5d5f59dd30
Greg <gregory.m.wagner@gmail.com>
2015-07-17 05:21:59 -0700

    added iit.r

---
+++ init.r
@@ -0,0 +1,25 @@
+if(!suppressWarnings(require(MCMCpack))) {
+  
+  install.packages("MCMCpack")
+  
+  library(MCMCpack)
+  
+} else {
+  
+  library(MCMCpack)
+  
+}
+
+
+
+if(!suppressWarnings(require(car))) {
+  
+  install.packages("car")
+  
+  library(car)
+  
+} else {
+  
+  library(car)
+  
+}
